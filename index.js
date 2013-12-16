2035be9e18bbcc700a3f5b6caac45d37e98df163
Nijiko Yonskai <nijikokun@gmail.com>
2013-12-16 15:04:19 -0800

    Re-work cross-platform modularity

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+// File-Size | 0.0.3 | MIT | Nijiko Yonskai <nijikokun@gmail.com> | 2013
 (function () {
   var filesize = function (bytes, options) {
     bytes = typeof bytes == 'number' ? bytes : 0;
 
     options = options || {};
-
     options.fixed = typeof options.fixed == 'number' ? options.fixed : 2;
     options.spacer = typeof options.spacer == 'string' ? options.spacer : ' ';
 
     var sizable = {
       calculate: function (SI) {
-        var algorithm = SI ? 1e3 : 1024;
-        var magnitude = Math.log(bytes) / Math.log(algorithm)|0;
-        var result = (bytes / Math.pow(algorithm, magnitude));
+        var algorithm = SI ? 1e3 : 1024
+          , magnitude = Math.log(bytes) / Math.log(algorithm)|0
+          , result = (bytes / Math.pow(algorithm, magnitude));
 
         return {
           magnitude: magnitude,
@@ -23,26 +23,23 @@
       to: function (unit, si) {
         unit = typeof unit == 'string' ? unit[0].toUpperCase() : 'B';
 
-        var algorithm = si ? 1e3 : 1024;
-        var units = 'BKMGTPEZY'.split('');
-        var position = units.indexOf(unit);
-        var result = bytes;
+        var algorithm = si ? 1e3 : 1024
+          , units = 'BKMGTPEZY'.split('')
+          , position = units.indexOf(unit)
+          , result = bytes;
 
         if (position == -1 || position == 0) return result;
         for (; position > 0; position--) result /= algorithm;
-
         return parseFloat(result.toFixed(options.fixed));
       },
 
       human: function (spec) {
         spec = spec || {};
         
-        var algorithm = spec.si ? ['k', 'B'] : ['K', 'iB'];
-        var input = sizable.calculate(spec.si);
+        var algorithm = spec.si ? ['k', 'B'] : ['K', 'iB']
+          , input = sizable.calculate(spec.si);
 
-        if (--input.magnitude < 2 && !spec.si && spec.jedec)
-          algorithm[1] = 'B';
-
+        if (--input.magnitude < 2 && !spec.si && spec.jedec) algorithm[1] = 'B';
         return input.fixed + options.spacer + (input.magnitude ? (algorithm[0] + 'MGTPEZY')[input.magnitude] + algorithm[1] : 'Bytes');
       }
     };
@@ -50,19 +47,14 @@
     return sizable;
   };
 
-  //commonjs/nodejs module
+  // Module Exporting
   if (typeof module !== 'undefined' && module.exports) {
     return module.exports = filesize;
-  }
-  
-  //amd module
-  if (typeof define === 'function' && define.amd) {
+  } else if (typeof define === 'function' && define.amd) {
     return define([], function() {
       return filesize;
     });
+  } else {
+    this.filesize = filesize;
   }
-  
-  //browser binding
-  this.filesize = filesize;
-  
 }());
